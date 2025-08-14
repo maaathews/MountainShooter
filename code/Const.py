d0c78fb08368fac76c111089cefc9acc72939cc7
Matheus Pereira <maaathews@hotmail.com>
2025-08-14 16:41:08 -0300

    Hit box collisions HUD Score

--- code/Const.py
+++ code/Const.py
@@ -1,9 +1,11 @@
 # C
 import pygame
 
-COLOR_ORANGE = (255, 128, 0)
-COLOR_WHITE = (255, 255, 255)
-COLOR_YELLOW = (255, 255, 0)
+C_ORANGE = (255, 128, 0)
+C_WHITE = (255, 255, 255)
+C_YELLOW = (255, 255, 0)
+C_GREEN = (0, 128,0)
+C_CYAN = (0, 128, 128)
 
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
@@ -16,13 +18,13 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 6,
-    'Player1'  : 3,
+    'Player1': 3,
     'Player1Shot': 1,
-    'Player2'  : 3,
+    'Player2': 3,
     'Player2Shot': 1,
-    'Enemy1'   : 1,
+    'Enemy1': 1,
     'Enemy1Shot': 3,
-    'Enemy2'   : 1,
+    'Enemy2': 1,
     'Enemy2Shot': 3,
 }
 
@@ -40,7 +42,7 @@ ENTITY_HEALTH = {
     'Player2Shot': 1,
     'Enemy1': 50,
     'Enemy1Shot': 5,
-    'Enemy2': 50,
+    'Enemy2': 60,
     'Enemy2Shot': 2,
 }
 
@@ -51,29 +53,64 @@ ENTITY_SHOT_DELAY = {
     'Enemy2': 200,
 }
 
+ENTITY_DAMAGE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    'Player1': 1,
+    'Player1Shot': 25,
+    'Player2': 1,
+    'Player2Shot': 20,
+    'Enemy1': 1,
+    'Enemy1Shot': 20,
+    'Enemy2': 1,
+    'Enemy2Shot': 15,
+
+}
+
+ENTITY_SCORE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    'Player1': 0,
+    'Player1Shot': 0,
+    'Player2': 0,
+    'Player2Shot': 0,
+    'Enemy1': 100,
+    'Enemy1Shot': 0,
+    'Enemy2': 125,
+    'Enemy2Shot': 0,
+}
 
 # M
 
-MENU_OPTION =  ('NEW GAME 1P',
-                'NEW GAME 2P - COOPERATIVE',
-                'NEW GAME 2P - COMPETITIVE',
-                'SCORE',
-                'EXIT')
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
 
 # P
 
-PLAYER_KEY_UP = {   'Player1': pygame.K_UP,
-                    'Player2': pygame.K_w}
-PLAYER_KEY_DOWN = { 'Player1': pygame.K_DOWN,
-                    'Player2': pygame.K_s}
-PLAYER_KEY_LEFT = { 'Player1': pygame.K_LEFT,
-                    'Player2': pygame.K_a}
+PLAYER_KEY_UP = {'Player1': pygame.K_UP,
+                 'Player2': pygame.K_w}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
+                   'Player2': pygame.K_s}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
+                   'Player2': pygame.K_a}
 PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
                     'Player2': pygame.K_d}
 PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
                     'Player2': pygame.K_LCTRL
-}
-
+                    }
 
 # S
 
